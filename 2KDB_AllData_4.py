125b3cfb1340cbc6b95229115eb38f899493fadd
tarek137 <tarek.lazaar@gmail.com>
2021-07-03 13:03:30 +0200

    working file 2kDB_allData_4

---
+++ 2KDB_AllData_4.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+import time
+from Data import Data
+from HomePage import HomePage
+from tqdm import tqdm
+
+players_links = []
+
+driver = webdriver.Chrome(executable_path="D:\\chromedriver.exe")
+homePage = HomePage(driver)
+data = Data(driver)
+
+homePage.OpenHomePage()
+
+homePage.AgreeHandle()
+
+nb = int(homePage.getnumberpages()[-4:-1])
+
+for j in tqdm(range(nb)):
+
+
+    players_list = homePage.getPlayersList()
+
+    for link in players_list:
+        players_links.append(link.get_attribute("href"))
+
+    for i in range(len(players_links)):
+
+        driver.get(players_links[i])
+        data.Collect_player_data()
+        data.Process_Data(data.Name, data.Color ,data.Team , data.Position , data.Speed , data.Shot_threepoint ,
+                          data.Pdefense,data.Idefense , data.Passing , data.Block, data.Steal ,data.Orebound ,
+                          data.Drebound)
+
+        time.sleep(3)
+
+    homePage.previousPage()
+    homePage.nextPage(j)
+    players_links =[]
+    print("Page" +str(j)+" done")
+    time.sleep(3)
+
+
+data.Store_dataframe()
+
+data.player_df.to_csv('test.csv')
+
+print(data.player_df.head(5))
+
+driver.close()
--- test.py
+++ test.py
@@ -8,42 +8,23 @@ list3 =[]
 color_list= ["bronze", "silver", "gold" ,"emerald","ruby","amethyst","heatcheck", "pink_diamond","diamond","galaxy_opal","dark_matter"]
 
 driver = webdriver.Chrome(executable_path="D:\\chromedriver.exe")
-driver.get("http://www.2kdb.net")
+driver.get("http://www.2kdb.net/players")
 driver.implicitly_wait(15)
 driver.maximize_window()
 time.sleep(5)
 try:
-        driver.find_element_by_xpath('//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]').click()
+        driver.find_element_by_xpath('//button[@aria-label="AGREE"]').click()
 
-except : NoSuchElementException
+except :
+    NoSuchElementException
 pass
+driver.refresh()
+
+button = driver.find_element_by_css_selector("div.p-d-flex.p-flex-column div.layout-with-topbar div.filterbox-main div.container div.p-datatable.p-component.p-datatable-auto-layout.p-datatable-hoverable-rows.p-datatable-sm.p-datatable-gridlines.table-players:nth-child(4) div.p-paginator.p-component.p-paginator-bottom > span.p-paginator-current").get_attribute("innerHTML")
+
+print(button[-4:-1])
 
 
-
-driver.find_element_by_xpath("//a[contains(text(),'Updates')]").click()
-
-driver.find_element_by_xpath("//body/div[@id='__next']/div[1]/div[3]/div[1]/nav[1]/div[1]/a[1]/span[1]/span[1]").click()
-
-
-time.sleep(5)
-
-list1 = driver.find_elements_by_tag_name("a")
-
-for val in list1 :
-    list2.append(val.get_attribute("href"))
-
-for i in range(len(list2)):
-    if "player/2k21" in str(list2[i]):
-        list3.append(list2[i])
-    else :
-        continue
-
-for i in range(len(list3)):
-    driver.get(list3[i])
-    time.sleep(5)
-    driver.back()
-
-driver.close()
 
 
 
